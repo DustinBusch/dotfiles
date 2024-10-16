2cb4be9adf3315cd9f94f49848330dafb3a931a3
DustinBusch <dustinbusch@yahoo.de>
2024-10-16 23:26:13 +0200

    First commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+########
+# nvim #
+########
+rm -rf "$HOME/.config/nvim"
+ln -s "$HOME/dotfiles/nvim" "$HOME/.config"
+
+
+rm -rf "$HOME/.config/X11"
+ln -s "$HOME/dotfiles/X11" "$HOME/.config"
