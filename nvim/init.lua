3514905c1be4dac9ebdc4b62c43c2c2d27ee6552
DustinBusch <dustinbusch@yahoo.de>
2024-10-23 14:01:35 +0200

    vimtex setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -230,7 +230,6 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -262,6 +261,12 @@ require('lazy').setup({
         -- VimTeX configuration goes here, e.g.
         vim.g.vimtex_view_method = 'zathura'
       end,
+    },
+    {
+      'gelguy/wilder.nvim',
+      opts = {
+        modes = { ':', '/', '?' },
+      },
     },
   },
 
@@ -614,9 +619,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
